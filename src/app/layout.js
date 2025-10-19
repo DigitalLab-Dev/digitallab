67add62bc4964f408e067df4317f4fb97366ca83
digitallab <digitallab514@gmail.com>
2025-10-20 01:11:26 +0500

    recovered

---
+++ src/app/layout.js
@@ -0,0 +1,31 @@
+import { Poppins } from 'next/font/google';
+import Navbar from '@/components/Navbar';
+import './globals.css';
+import SplashCursor from '@/components/ReactBit-Components/SplashCursor';
+import Footor from '@/components/Footer';
+import ScrollToTop from '@/components/ScrollToTop';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-poppins',
+});
+
+export const metadata = {
+  title: 'DigitalLab',
+  description: 'DigitalLab - A service providing agency',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.variable} font-poppins antialiased`}>
+        <Navbar />
+        {children}
+        <ScrollToTop/>
+        <Footor />
+        {/* <SplashCursor/> */}
+      </body>
+    </html>
+  );
+}
